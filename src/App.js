cd129524fab15741bb0b4fccb88cab8141d5e322
wx0000 <w.szaron@gmail.com>
2020-02-07 14:42:25 +0100

    added shop page collection preview

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import {Switch,  Route} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './page/homepage/homepage.component';
+import ShopPage from './page/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage}/>
-        <Route path="/hats" component={HatsPage}/>
+        <Route path="/shop" component={ShopPage}/>
       </Switch>
     </div>
   );
